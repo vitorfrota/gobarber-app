def063e7fd2c456f1a2fd6add387beb2d19ce5fd
Vitor Frota <vitorfrota@live.com>
2020-06-01 09:59:00 -0400

    ...4 - Configurando navegação

---
+++ src/pages/Signin/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Signin: React.FC = () => {
+  const a = 0;
+  return (
+    <Container />
+  );
+};
+
+export default Signin;
---
+++ src/pages/Signup/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Signup: React.FC = () => {
+  const a = 0;
+  return (
+    <Container />
+  );
+};
+
+export default Signup;
