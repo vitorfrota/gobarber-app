1f1432e4626176a8fc98242b2e3b5c26d935f980
Vitor Frota <vitorfrota@live.com>
2020-06-01 10:06:02 -0400

    5 - Densidade de pixel

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import { Image } from 'react-native';
 import { Container } from './styles';
+
+import logoImg from '../../assets/logo.png';
 
 const Signin: React.FC = () => {
   const a = 0;
   return (
-    <Container />
+    <Container>
+      <Image source={logoImg} />
+    </Container>
   );
 };
 
