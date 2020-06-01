a45f68a25e40c2efc9d91a3abef5a21d93e6cd77
Vitor Frota <vitorfrota@live.com>
2020-06-01 15:45:54 -0400

    7 - Input & Button

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Image } from 'react-native';
-import { Container, Title } from './styles';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
 import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const Signin: React.FC = () => {
   const a = 0;
@@ -10,6 +14,17 @@ const Signin: React.FC = () => {
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+      <Input
+        name="email"
+        icon="mail"
+        placeholder="E-mail"
+      />
+      <Input
+        name="password"
+        icon="lock"
+        placeholder="Senha"
+      />
+      <Button onPress={() => { console.log('deu'); }}>Entrar</Button>
     </Container>
   );
 };
