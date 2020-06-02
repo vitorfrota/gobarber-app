f4aa7bd24dfa201627cc33988b4daf406408778d
Vitor Frota <vitorfrota@live.com>
2020-06-01 22:01:59 -0400

    12 - Foco e preenchimento

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,16 +1,24 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 
-export const Container = styled.View`
+interface ContainerProps {
+  isFocused: boolean;
+}
+
+export const Container = styled.View<ContainerProps>`
   width: 100%;
   height: 50px;
   padding: 0 16px;
   background: #242028;
   border-radius: 10px;
   margin-bottom: 8px;
+  border-width: 2px;
+  border-color: #232129;
 
   flex-direction: row;
   align-items: center;
+
+  ${(props) => props.isFocused && css`border-color: #ff9900`};
 `;
 
 export const TextInput = styled.TextInput`
