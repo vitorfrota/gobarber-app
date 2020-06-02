b15d9b379dcdef8d637c305245ea69952ba0cee0
Vitor Frota <vitorfrota@live.com>
2020-06-01 23:13:40 -0400

    16 - Rotas privadas

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -18,12 +18,14 @@ interface AuthContextData {
   user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
+  loading: boolean;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStoragedData(): Promise<void> {
@@ -36,6 +38,7 @@ const AuthProvider: React.FC = ({ children }) => {
       if (token[1] && user[1]) {
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
+      setLoading(false);
     }
     loadStoragedData();
   }, []);
@@ -63,7 +66,10 @@ const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{
+      user: data.user, signIn, loading, signOut,
+    }}
+    >
       { children }
     </AuthContext.Provider>
   );
